b0c7e98ff6c359020dee1403d7ba4a6bd153afa6
jreleaserbot <jreleaser@kordamp.org>
2026-08-27 11:33:14 +0000

    jenesis v0.10.9

--- Formula/jenesis.rb
+++ Formula/jenesis.rb
@@ -3,9 +3,9 @@
 class Jenesis < Formula
   desc "A Java-native build tool."
   homepage "https://github.com/raphw/jenesis"
-  url "https://github.com/raphw/jenesis/releases/download/v0.10.8/jenesis-0.10.8.zip"
-  version "0.10.8"
-  sha256 "a9be20857cec192b819d8236f5863577fd6b29c368a542f14c42751f0f54e667"
+  url "https://github.com/raphw/jenesis/releases/download/v0.10.9/jenesis-0.10.9.zip"
+  version "0.10.9"
+  sha256 "4b2864162159e4b8031fdc6fd266d704df7dff86a64d157a2b431fab63c0cf54"
   license "Apache-2.0"
 
   depends_on "openjdk@25"
@@ -17,6 +17,6 @@ class Jenesis < Formula
 
   test do
     output = shell_output("#{bin}/jenesis --version")
-    assert_match "0.10.8", output
+    assert_match "0.10.9", output
   end
 end
